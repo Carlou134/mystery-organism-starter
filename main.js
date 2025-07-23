c70578968050635523b0754c3a0db4bb33ce8c11
Carlos Vásquez Rodriguez <81259501+Carlou134@users.noreply.github.com>
2025-07-23 12:43:02 -0500

    Ultimos cambios

--- main.js
+++ main.js
@@ -16,12 +16,83 @@ const mockUpStrand = () => {
 function pAequorFactory(specimenNum, dna){
   return{
     specimenNum,
-    dna
+    dna,
+    mutate(){
+      const randomIndex = Math.floor(Math.random() * this.dna.length);
+      const currentBase = this.dna[randomIndex];
+      let newBase = returnRandBase();
+      while (newBase === currentBase) newBase = returnRandBase();
+      this.dna[randomIndex] = newBase;
+      return this.dna;
+    },
+    compareDNA(pAequor){
+      let identicalBases = 0;
+      let identicalPositions = [];
+      
+      for(let i = 0; i < this.dna.length; i++){
+        if(this.dna[i] === pAequor.dna[i]) {
+          identicalBases++;
+          identicalPositions.push(i);
+        }
+      }
+
+      let percentage = (identicalBases / this.dna.length) * 100;
+      console.log(`specimen ${this.specimenNum} and specimen ${pAequor.specimenNum} have ${percentage}% DNA in common`);
+    },
+    willLikelySurvive(){
+      let cont = 0;
+      this.dna.forEach(element => {
+        if(element === 'C' || element === 'G') cont++;
+      });
+
+      let percentage = (cont / this.dna.length) * 100;
+      return (percentage >= 60.0);
+    },
+    complementStrand(){
+      const newDna = this.dna.map(element => {
+        switch(element){
+          case 'A': element = 'T'; break;
+          case 'T': element = 'A'; break;
+          case 'C': element = 'G'; break;
+          case 'G': element = 'C'; break;
+        }
+        return element;
+      })
+      return newDna;
+    }
   }
 }
 
+//test 1
+const object = pAequorFactory('12222', mockUpStrand());
+console.log(object);
+console.log(object.mutate());
 
+//test 2
+const ex1 = pAequorFactory('ex1', ['A', 'C', 'T', 'G']);
+const ex2 = pAequorFactory('ex1', ['C', 'A', 'T', 'T']);
 
+ex1.compareDNA(ex2);
 
+//test 3
+const arrayInstances = [];
+let i = 0;
 
+while(i < 30){
+  let variable = pAequorFactory(`Specimen ${i + 1}`, mockUpStrand());
+  if(variable.willLikelySurvive()){
+    arrayInstances.push(variable);
+    i++;
+  }
+}
 
+console.log(arrayInstances);
+
+//test optional
+const objectOptional = pAequorFactory('objectOptional', [ 'T', 'A', 'C', 'A', 'G', 'A', 'T', 'A', 'C', 'G', 'A', 'C', 'G', 'A', 'T' ]);
+console.log(objectOptional);
+
+const complementDna = objectOptional.complementStrand();
+const objComplement = pAequorFactory('ComplementObject', complementDna);
+
+objectOptional.compareDNA(objComplement);
